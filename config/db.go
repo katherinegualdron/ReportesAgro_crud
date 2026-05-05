955a3ed815d502f23c60c065b9e216f1e29595bc
Katha Gualdrón <gualdronmarquezw2008@gmail.com>
2026-05-04 19:19:00 -0500

    Cambios

--- config/db.go
+++ config/db.go
@@ -14,9 +14,9 @@ func ConnectDB() {
 	host := "localhost"
 	port := 5432
 	user := "postgres"
-	password := "transversal10"
+	password := "postgres"
 	dbname := "AgroCampo"
-	schema := "\"Reportes\""
+	schema := "Reportes"
 
 	psqlInfo := fmt.Sprintf(
 		"host=%s port=%d user=%s password=%s dbname=%s search_path=%s sslmode=disable",
